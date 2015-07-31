b4609616886e0a6ea3b5d6fd96d1c59660b1f5fb
pabloem <polecito.em@gmail.com>
2015-07-31 18:21:24 +0900

    Work on regenerating the database. Also, fixed jshint run to include only ldp-server

--- bin/consolidate.sh
+++ bin/consolidate.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ "$#" -eq 4 ]; then
+    cp -r $1 $1_1
+    cp -r $2 $2_1
+fi
 cd ../hdt-iris/ # // jshint ignore:line
 ./bin/consolidate.sh $*  # // jshint ignore:line
-
+if [ "$#" -eq 4 ]; then
+    rm -r $1_1
+    rm -r $2_1
+fi
